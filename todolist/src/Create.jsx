4418f7f8448e0bd4a824d397330c52c49b97f2a7
YashDhoke <yashdhoke1@gmail.com>
2024-01-12 03:06:05 +0530

    initial

---
+++ todolist/src/Create.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Create() {
+  return (
+    <div>
+        <input type="text" />
+        <button type = "button">Add</button>
+    </div>
+  )
+}
+
+export default Create
